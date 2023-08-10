447226f98a424d4fc3d8327a29e1797b14505b61
Ryujikjs <ryujikjs@gmail.com>
2023-08-11 00:23:56 +0800

    Add links to resume and portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -30,14 +30,14 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
       </Head>
 
-      <div className='flex pt-10'>
+      <div className='flex'>
         <main className='bg-white px-20 font-body'>
           <section className="min-h-screen pb-52 pt-20">
             <div>
               <Image src={ryuji} alt="Ryuji Kow" width={300} height={300} className='rounded-3xl mb-10 border-4 border-black' />
               <h1 className='text-8xl font-bold pb-3'>Ryuji</h1>
               <h1 className='text-8xl font-bold pb-3'>Kow Jie Si</h1>
-              <h2>Computer Engineering Student</h2>
+              <h2 className='text-2xl'>Computer Engineering Student</h2>
             </div>
           </section>
 
@@ -210,7 +210,7 @@ export default function Home() {
               verifying the compatibility of selected components.</p>
               <br></br>
               <Image src={bigpp} alt="Big PC-Partpicker" width={500} height={300} className='rounded-xl justify-center mx-auto my-8' />
-              <p>As a collaborative team effort, we proudly present Big PC Part-Picker (BigPP), a command-line application crafted with passion for PC enthusiasts and dedicated builders. Our goal is to empower users with a seamless and personalized PC building experience. BigPP streamlines the process of selecting and verifying compatible components, offering a time-saving and cost-effective solution. With a user-friendly interface, it enables easy customization and organization of PC builds, ensuring optimal performance and budget adherence.</p>
+              <p>Big PC Part-Picker (BigPP) is a command-line application crafted with passion for PC enthusiasts and dedicated builders. Our goal is to empower users with a seamless and personalized PC building experience. BigPP streamlines the process of selecting and verifying compatible components, offering a time-saving and cost-effective solution. With a user-friendly interface, it enables easy customization and organization of PC builds, ensuring optimal performance and budget adherence.</p>
               <div className="mt-6">
                 <h3 className="text-lg font-semibold">Key Highlights:</h3>
                 <ul className="list-inside mt-2">
@@ -219,7 +219,7 @@ export default function Home() {
                   <li className="list-disc">Enhanced customizability for advanced users to add custom pc parts to the list of available parts.</li>
                 </ul>
               </div>
-              <p><a href="https://github.com/AY2223S2-CS2113-T12-2/tp" class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline pt-4">
+              <p><a href="https://ay2223s2-cs2113-t12-2.github.io/tp/" class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline pt-4">
               Check out the app
                 <svg class="w-4 h-4 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -377,38 +377,40 @@ export default function Home() {
             </li>
             
           </ul>
-
           <p>ryujikjs@gmail.com</p>
           <p className='pb-3'>+65 9727 8031</p>
-
-          <div className='items-center justify-center'>
-          <a href="https://www.linkedin.com/in/ryuji-kow-224894227/" role="button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-7 w-7"
-              fill="#0077b5"
-              viewBox="0 0 24 24">
-              <path
-                d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-            </svg>
-          </a>
-          <br></br>
-          <a href="https://github.com/Ryujikjs" role="button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-7 w-7"
-              fill="#333"
-              viewBox="0 0 24 24">
-              <path
-                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
-          </a>
-
-          </div>
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-40 ">
+            <a href="Ryuji Kow CV.pdf" alt="pdf">Resume PDF</a>
+          </button>
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+            <a href="Ryuji's Projects.pdf" alt="projects pdf">Projects PDF</a>
+          </button>
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+            <a href="https://github.com/Ryujikjs" className='mx-9'>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-7 w-7"
+                fill="#333"
+                viewBox="0 0 24 24">
+                <path
+                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
+            </a>
+          </button>
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+            <a href="https://www.linkedin.com/in/ryuji-kow-224894227/" className='mx-9'>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-7 w-7"
+                fill="#0077b5"
+                viewBox="0 0 24 24">
+                <path
+                  d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+              </svg>
+            </a>
+          </button>
         </nav>
-
       </div>
-
     </div>
   )
 }
